744f5cac6448ff5ee981576e2a44a91804c3460e
alekspashinin <alekspashinin@gmail.com>
2021-01-13 01:22:01 +0100

    Last Commits

---
+++ src/test/java/com/emse/spring/faircorp/dao/BuildingDaoTest.java
@@ -0,0 +1,38 @@
+/**
+ *
+ *                      UJM * EMSE
+ *
+ *                  * Aleksei PASHININ *
+ *
+ *                     WMP Project
+ *
+ */
+
+package com.emse.spring.faircorp.dao;
+
+
+import com.emse.spring.faircorp.model.Window;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
+public class BuildingDaoTest {
+    @Autowired
+    BuildingDao buildingDao;
+
+    @Test
+    public void shouldFindWindows() {
+        List<Window> result = buildingDao.findWindowInBuilding(1L);
+        Assertions.assertThat(result)
+                .hasSize(2)
+                .extracting("id")
+                .containsExactly(1L,2L);
+    }
+}
